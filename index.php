e179d57ca627051976f8bb16df4a32432ee17f2b
Ivan Cegledi <ivan.cegledi1@skole.hr>
2016-03-08 17:33:23 +0100

    Procistili smo PHP

--- index.php
+++ index.php
@@ -4,29 +4,12 @@
 
     <meta charset="UTF-8">
 
-<title>Naslov</title>
+<title>Forme</title>
 </head>
 <body>
-<h1>Prvi</h1>
+<h1>Forme</h1>
 
-<p>
-    <?php
-    echo('Pozdrav iz PHP-a');
-    echo('<br>');
-    echo(date('d.m.Y.'))
 
-    ?>
-</p>
-<p>
-    <?php
-    $dana_do_praznika = 45;
-    echo('Do praznika ja ostalo još:<b>' . $dana_do_praznika . '</b>');
-    $date1 = date("Y-m-d");
-    $date2 = date("2016-03-18");
-    $difference = date_diff($date1, $date2);
-
-    ?>
-</p>
 
 
 </body>
